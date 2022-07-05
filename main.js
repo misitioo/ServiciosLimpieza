833d39bc0589f9418fadbda07f48b64100451f2b
misitioo <dayananimax@gmail.com>
2022-07-04 23:37:44 -0400

    arreglo

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function menuAbrir(){
     document.querySelector('.box-menu').style.display = "block";
 }
+
+function btnClose(){
+    const btnCloseBox = document.querySelector('.btn-close');
+    document.querySelector('.box-menu').style.display = "none";
+}   
